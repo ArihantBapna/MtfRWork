72033ba1717338db21d47f49ed26532ce83bbb4c
AriB0611 <bapna.arihant02@gmail.com>
2020-02-16 10:21:41 -0500

    CountyAnalysis

--- AnnualReportAnalysis.R
+++ AnnualReportAnalysis.R
@@ -1,9 +1,13 @@
 p <- c()
 
 for(i in levels(factor(AnnualReport$`County Name`))){
-  db <- select(filter(AnnualReport$`County Name`==i), c(`Commodity Year`,`Liabilities`, `Indemnity`))
-  sumLoss <- sum(db$`Indemnity`)
-  sumLib <- sum(db$`Liabilities`)
+  db <- select(filter(AnnualReport,str_detect(AnnualReport$`County Name`,i)), c(`Commodity Year`,`Liabilities ($)`, `Indemnity ($)`))
+  sumLoss <- sum(db$`Indemnity ($)`)
+  sumLib <- sum(db$`Liabilities ($)`)
   x <- (sumLib - sumLoss)/(sumLib)
   p <- c(p,x)
 }
+df <- data.frame("County" = levels(factor(AnnualReport$`County Name`)), "Percent.Loss.Incurred" = p )
+plot <- plot_ly(f,x=~County, y=~Percent.Loss.Incurred, orientation='h')
+
+remove(i,p,db,sumLoss,sumLib,x)
